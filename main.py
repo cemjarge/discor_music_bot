50484073a6763cb5b0960976bf1ff8dac13b10e1
Jarge Cem Karadag <cemjarge@gmail.com>
2022-12-04 19:49:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import os
+import asyncio
+import nest_asyncio
+
+nest_asyncio.apply()
+
+#import all of the cogs
+from help_cog import help_cog
+from music_cog import music_cog
+
+bot = commands.Bot(command_prefix='/', intents = discord.Intents.all())
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+#register the class with the bot
+async def main():
+    async with bot:
+        await bot.add_cog(help_cog(bot))
+        await bot.add_cog(music_cog(bot))
+        await bot.run("MTA0ODk2NDEyOTQwNDIzNTgwNg.GhVD77.Yms-t0c91n_oHGehe50BSK_YA5axZHV43vE6_0")
+
+asyncio.run(main())
+
